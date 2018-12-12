ad8efc52bcf48a067acdd3117ce9e364ee460e98
MichaelM3 <dagreekarican@aim.com>
2018-12-12 11:16:42 -0500

    Done.

--- app.rb
+++ app.rb
@@ -5,9 +5,8 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/piglatinizer' do
-    @analyzed_text = PigLatinizer.new(params[:user_text])
-
-    erb :piglatinizer
+  post '/piglatinize' do
+    @analyzed_text = PigLatinizer.new.piglatinize(params[:user_phrase])
+    erb :piglatinize
   end
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,19 +1,17 @@
 class PigLatinizer
 
-  attr_reader :text
-
-  def initialize(text)
-    @text = text.downcase
-  end
-
-  def translation
-    alpha = ('a'..'z').to_a
-    vowels = %w[a e i o u]
-    consonants = alpha - vowels
-    arrayOfText = text.split
+  def piglatinize(string)
+    downAlpha = ('a'..'z').to_a
+    upAlpha = ('A'..'Z').to_a
+    vowels = %w[a e i o u A E I O U]
+    entireAlpha = downAlpha + upAlpha
+    consonants = entireAlpha - vowels
+    arrayOfText = string.split
     pigLatinTranslator = arrayOfText.map do |word|
       if vowels.include?(word[0])
-        word + 'ay'
+        word + 'way'
+      elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+        word[3..-1] + word[0..2] + 'ay'
       elsif consonants.include?(word[0]) && consonants.include?(word[1])
         word[2..-1] + word[0..1] + 'ay'
       elsif consonants.include?(word[0])
@@ -22,6 +20,7 @@ class PigLatinizer
         word
       end
     end
-    pigLatinTranslator.join(" ").capitalize
+    pigLatinTranslator.join(" ")
   end
+
 end
